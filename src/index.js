2a2d31d8460a1e7f7e162df200038852f96090bd
이시형 <tlgud585@gmail.com>
2022-09-18 03:42:23 +0900

    fix : store 전달 Provider를 통해 하위 컴포넌트에 store 전달, 기존 store 삭제

--- src/index.js
+++ src/index.js
@@ -2,29 +2,19 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import { applyMiddleware, createStore } from "redux";
 import App from "./App";
-import rootReducer from "./modules";
-import reportWebVitals from "./reportWebVitals";
-import logger from "redux-logger";
-import ReduxThunk from "redux-thunk";
-
-const store = createStore(rootReducer, applyMiddleware(ReduxThunk, logger));
+import store from "./store";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
   <BrowserRouter>
-    <React.StrictMode>
+    {/* <React.StrictMode> */}
       <Provider store={store}>
         <App />
       </Provider>
-    </React.StrictMode>
+    {/* </React.StrictMode> */}
   </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
