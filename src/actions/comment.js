04161eacbb1a7b3c911f5705067575cea6513b50
이시형 <tlgud585@gmail.com>
2022-09-18 03:36:58 +0900

    feat : comment action comment action type 추가 , comment action creator 추가

---
+++ src/actions/comment.js
@@ -0,0 +1,62 @@
+import {
+	GET_COMMENTS,
+	CREATE_COMMENT,
+	UPDATE_COMMENT,
+	DELETE_COMMENT,
+} from "./types";
+
+import commentServices from "../services/comment";
+
+export const createComment =
+	(comment) => async (dispatch) => {
+		try {
+			const res = await commentServices.create(comment);
+			dispatch({
+				type: CREATE_COMMENT,
+				payload: res.data,
+			});
+			return Promise.resolve(res.data);
+		} catch (err) {
+			return Promise.reject(err);
+		}
+	};
+
+export const getComments = () => async (dispatch) => {
+	try {
+		const res = await commentServices.getAll();
+		dispatch({
+			type: GET_COMMENTS,
+			payload: res.data,
+		});
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+export const updateComment = (commentId, updatedComment) => async (dispatch) => {
+	try {
+		const res = await commentServices.update(commentId, updatedComment);
+
+		dispatch({
+			type: UPDATE_COMMENT,
+			payload: updatedComment,
+		});
+
+		return Promise.resolve(res.data);
+	} catch (err) {
+		return Promise.reject(err);
+	}
+};
+
+export const deleteComment = (commentId) => async (dispatch) => {
+	try {
+		await commentServices.delete(commentId);
+
+		dispatch({
+			type: DELETE_COMMENT,
+			payload: { commentId },
+		});
+	} catch (err) {
+		console.log(err);
+	}
+};
