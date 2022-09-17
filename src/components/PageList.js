e7770a10ecb4c9144324f7c536e8ea6c4553cf7a
이시형 <tlgud585@gmail.com>
2022-09-18 05:59:23 +0900

    feat : pagination 구현 전체 댓글 개수를 이용해 pagination 구현, page 클릭시 dispatch로 page action creator 전달 , page style 수정

--- src/components/PageList.js
+++ src/components/PageList.js
@@ -1,35 +1,58 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import styled from "styled-components";
+import { selectPage } from "../actions/page";
 
 const PageListStyle = styled.div`
-  margin-bottom: 20px;
-  text-align: center;
+	margin-bottom: 20px;
+	text-align: center;
 `;
 
 const Page = styled.button`
-  padding: 0.375rem 0.75rem;
-  border-radius: 0.25rem;
-  font-size: 1rem;
-  line-height: 1.5;
-  border: 1px solid lightgray;
-  ${({ active }) =>
-    active &&
-    `
+	padding: 0.375rem 0.75rem;
+	border-radius: 0.25rem;
+	font-size: 1rem;
+	line-height: 1.5;
+	border: 1px solid lightgray;
+	${({ active }) =>
+		active
+			? `
         background: gray;
         color: #fff;
-  `}
-  margin-right: 3px;
+        `
+			: `cursor: pointer;`}
+	margin-right: 3px;
 `;
 
 function PageList() {
-  const pageArray = [];
+	const commentsLen = useSelector((state) => state.comment).length;
+  const pageNumber = useSelector(state => state.page)
+  const dispatch = useDispatch()
 
-  pageArray.push(
-    // 임시로 페이지 하나만 설정했습니다.
-    <Page key="1">1</Page>
-  );
+  const pageArray = Array.from(
+		{ length: Math.ceil(commentsLen / 4) },
+		(_, idx) => idx
+	);
 
-  return <PageListStyle>{pageArray}</PageListStyle>;
+	const pageClickHandler = (page) => {
+		dispatch(selectPage(page))
+	};
+
+	return (
+		<PageListStyle>
+			{pageArray.map((page) => (
+				<Page
+					key={page}
+					active={page === pageNumber}
+					onClick={() => pageClickHandler(page)}
+          disabled={page === pageNumber}
+				>
+					{page+1}
+				</Page>
+			))}
+		</PageListStyle>
+	);
 }
 
 export default PageList;
