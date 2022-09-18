5650cdf207896b00e2e6cbebc6a0d70bc4ae405d
이시형 <tlgud585@gmail.com>
2022-09-18 14:38:20 +0900

    fix : form state 관리 수정 , 댓글 정보 받아오기 추가 form state ref로 관리하던거 useState로 변경, id을 이용해 댓글 정보 받아오기 추가

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,12 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import styled from "styled-components";
-import { createComment } from "../actions/comment";
+import { createComment, updateComment } from "../actions/comment";
 import { selectPage } from "../actions/page";
+
+import CommentServices from "../services/comment";
 
 const FormStyle = styled.div`
 	& > form {
@@ -28,58 +32,91 @@ const FormStyle = styled.div`
 `;
 
 function Form() {
-	const formRef = useRef();
-	const imgRef = useRef();
-	const authorRef = useRef();
-	const contentRef = useRef();
-	const createadRef = useRef();
+	const [formData, setFormData] = useState({
+		profile_url: "",
+		author: "",
+		content: "",
+		createdAt: "",
+	});
+
+	const commentId = useSelector((state) => state.comment.selectedCommentId);
+	useEffect(() => {
+		const getComment = async () => {
+			const res = await CommentServices.get(commentId);
+			setFormData(res.data);
+		};
+		commentId && getComment();
+	}, [commentId]);
 
 	const dispatch = useDispatch();
 
 	const submitHandler = (e) => {
 		e.preventDefault();
-		const comment = {
-			profile_url: imgRef.current.value,
-			author: authorRef.current.value,
-			content: contentRef.current.value,
-			createdAt: createadRef.current.value,
-		};
-		dispatch(createComment(comment));
-		dispatch(selectPage(0));
+		if (!commentId) {
+			console.log(formData);
+			dispatch(createComment(formData));
+			dispatch(selectPage(0));
+		} else dispatch(updateComment(commentId, formData));
 
-		formRef.current.reset();
+		setFormData({
+			profile_url: "",
+			author: "",
+			content: "",
+			createdAt: "",
+		});
 	};
 
 	return (
 		<FormStyle>
-			<form ref={formRef} onSubmit={submitHandler}>
+			<form onSubmit={submitHandler}>
 				<input
-					ref={imgRef}
 					type="text"
 					name="profile_url"
 					placeholder="https://picsum.photos/id/1/50/50"
+					value={formData.profile_url}
+					onChange={(e) =>
+						setFormData((pre) => {
+							return { ...pre, profile_url: e.target.value };
+						})
+					}
 					required
 				/>
 				<br />
 				<input
-					ref={authorRef}
 					type="text"
 					name="author"
 					placeholder="작성자"
+					value={formData.author}
+					onChange={(e) =>
+						setFormData((pre) => {
+							return { ...pre, author: e.target.value };
+						})
+					}
+					required
 				/>
 				<br />
 				<textarea
-					ref={contentRef}
 					name="content"
 					placeholder="내용"
+					value={formData.content}
+					onChange={(e) =>
+						setFormData((pre) => {
+							return { ...pre, content: e.target.value };
+						})
+					}
 					required
 				></textarea>
 				<br />
 				<input
-					ref={createadRef}
 					type="date"
 					name="creataedAt"
 					placeholder="2022-09-18"
+					value={formData.createdAt}
+					onChange={(e) =>
+						setFormData((pre) => {
+							return { ...pre, createdAt: e.target.value };
+						})
+					}
 					min="2022-09-18"
 					// value="2022-09-18"
 					required
